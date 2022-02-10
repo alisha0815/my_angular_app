125ffce609c8c62dfcb92d4a77a5f92c89dc82d9
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-02-10 11:21:43 +0100

    change css by using setclasses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,4 +15,12 @@ export class HomeComponent implements OnInit {
   counterClick() {
     this.clickCounter += 1;
   }
+
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4,
+    };
+    return myClasses;
+  }
 }
