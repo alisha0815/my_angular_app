66d91a3fbc05771eba0acd4af7b6ba2fd04a724d
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-02-10 11:33:52 +0100

    connect service

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,15 @@
+import { HttpService } from './../http.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  constructor(private _http: HttpService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this._http.myMethod();
   }
-
 }
